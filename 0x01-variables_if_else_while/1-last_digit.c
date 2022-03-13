be30cf0192c497b1f07432987ce44c6568601056
Bethel-tech <osahonb1@gmail.com>
2022-03-13 10:30:03 -0700

    Making corrections to tasks 0, 1, and 4 that needed space before the parenthesis

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,5 +20,5 @@ int main(void)
 		printf("and is 0\n");
 	else if (n % 10 < 6 && n % 10 != 0)
 		printf("and is less than 6 and not 0\n");
-	return(0);
+	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -18,5 +18,5 @@ int main(void)
 		ch++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
